e224babd1eb8c6f6580b4bd3bf2f940b0d11356f
Metti <107748176+Mettiuy@users.noreply.github.com>
2023-06-12 14:09:12 +0430

    get and convert products handled

--- shopping cart/app.js
+++ shopping cart/app.js
@@ -1,30 +1,21 @@
 let cartItemsContainer = document.querySelector(".left__carts-container");
-let scrolledHeight;
-document.body.addEventListener("keyup", (e) => {
-  let { key } = e;
 
-  if (key === "Enter") {
-    cartItemsContainer.insertAdjacentHTML(
-      "beforeend",
-      `
-          <div class="cart-item">
-            <div class="cart-img">
-              <img src="" alt="" class="cart__img" />
-            </div>
-            <div class="cart-infos">
-              <div class="info__category">Cloths</div>
-              <div class="info__name">Cotton T-shirt</div>
-            </div>
-            <div class="cart-count">
-              <input type="number" value="1" class="count__input" />
-            </div>
-            <div class="cart-price">$1212</div>
-            <button class="cart-close">X</button>
-          </div>
-`
-    );
-  }
-
-  scrolledHeight = cartItemsContainer.scrollHeight;
-  cartItemsContainer.scrollTo(scrolledHeight, scrolledHeight);
+window.addEventListener("load", () => {
+  fetch("https://shop-project1-d3570-default-rtdb.firebaseio.com/products.json")
+    .then((res) => res.json())
+    .then((data) => {
+      convertProducts(data);
+    });
 });
+
+function convertProducts(data) {
+  if (data) {
+    data = Object.entries(data);
+
+    data.forEach((product) => {
+      createProducts(product)
+    });
+  } else {
+    console.log("Empty");
+  }
+}
