d5b61ad1ce3f31958e0b0bafe3e7e3e47815b409
Metti <107748176+Mettiuy@users.noreply.github.com>
2023-06-12 14:14:42 +0430

    create products in DOM handled

--- shopping cart/app.js
+++ shopping cart/app.js
@@ -11,11 +11,34 @@ window.addEventListener("load", () => {
 function convertProducts(data) {
   if (data) {
     data = Object.entries(data);
+    cartItemsContainer.innerHTML = "";
 
     data.forEach((product) => {
-      createProducts(product)
+      createProducts(product[1]);
     });
   } else {
     console.log("Empty");
   }
+}
+
+function createProducts(product) {
+  cartItemsContainer.insertAdjacentHTML(
+    "beforeend",
+    `
+  <div class="cart-item">
+  <div class="cart-img">
+      <img src="${product.images[0]}" alt="" class="cart__img" />
+  </div>
+  <div class="cart-infos">
+      <div class="info__category">${product.category}</div>
+      <div class="info__name">${product.title}</div>
+  </div>
+  <div class="cart-count">
+      <input type="number" value="1" class="count__input" />
+  </div>
+  <div class="cart-price">$${product.price}</div>
+  <button class="cart-close">X</button>
+</div>
+  `
+  );
 }
